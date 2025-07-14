734df9788c536a96ea94502437b735a417f76d1b
ajessiah <jessyahanderson@gmail.com>
2025-07-14 13:36:20 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,127 @@
+const content = document.getElementById('page-content');
+const logoDiv = document.getElementById('logo-container');
+const homeBtn = document.getElementById('home-btn');
+const aboutBtn = document.getElementById('about-btn');
+const portfolioBtn = document.getElementById('portfolio-btn');
+const ratesBtn = document.getElementById('rates-btn');
+const contactBtn = document.getElementById('contact-btn');
+const portfolioDiv = document.getElementById('portfolio-container');
+const hamMenuBtn = document.getElementById('hamburger-btn');
+const hamMenu = document.getElementById('menu-overlay');
+const hamHome = document.getElementById('ham-home-btn');
+const hamAbout = document.getElementById('ham-about-btn');
+const hamPortfolio = document.getElementById('ham-portfolio-btn');
+const header = document.getElementById('header-div');
+
+const allImgs = ["Photos/nature.jpg", "Photos/nature2.jpg", "Photos/nature3.jpg", "Photos/nature4.jpg", "Photos/nature5.jpg", "Photos/nature6.jpg"];
+
+const toHome = () => {
+    content.classList.add('loading');
+    fetch("Pages/home.txt")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok: " + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            setTimeout(() => {
+            content.className = 'home';
+            content.innerHTML = data;
+            content.classList.remove('loading');
+            }, 200);
+        })
+        .catch(error => {
+            console.error('Error loading the text file:', error);
+        });
+};
+
+//window.onload = toHome;
+homeBtn.addEventListener("click", toHome);
+logoDiv.addEventListener("click", toHome);
+hamHome.addEventListener("click", toHome);
+
+
+const toAbout = () => {
+    content.classList.add('loading');
+    fetch("Pages/about.txt")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok: " + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            setTimeout(() => {
+            content.style.height = "60vh";
+            content.className = 'about';
+            content.innerHTML = data;
+            content.classList.remove('loading');
+            }, 200);
+        })
+        .catch(error => {
+            console.error('Error loading the text file:', error);
+        });
+};
+
+toAbout();
+
+aboutBtn.addEventListener("click", toAbout);
+hamAbout.addEventListener("click", toAbout);
+
+const popPortfolio = () => {
+    allImgs.forEach((img) => {
+        portfolioDiv.innerHTML += `
+            <div id="${img}" class="img-container">
+                <img src="${img}" class="portfolio-img">
+            </div>
+        `;
+    })
+};
+
+const toPortfolio = () => {
+    content.classList.add('loading');
+    fetch("Pages/portfolio.txt")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok: " + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            setTimeout(() => {
+                content.className = 'portfolio';
+                content.innerHTML = data;
+                const portfolioDiv = document.getElementById('portfolio-container');
+                allImgs.forEach((img) => {
+                    portfolioDiv.innerHTML += `
+                        <div id="${img}" class="img-container">
+                            <img src="${img}" class="portfolio-img">
+                        </div>
+                    `;
+                });
+                content.style.height = "fit-content";
+                content.classList.remove('loading');
+            }, 200);
+        })
+        .catch(error => {
+            console.error('Error loading the text file:', error);
+        });
+};
+
+portfolioBtn.addEventListener("click", toPortfolio);
+hamPortfolio.addEventListener("click", toPortfolio);
+
+hamMenuBtn.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+});
+
+const overlayButtons = document.querySelectorAll(".overlay-btns");
+
+overlayButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    hamMenu.classList.remove("active");
+  });
+});
+
+
